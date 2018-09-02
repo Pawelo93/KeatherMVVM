9ada8bc9ccf89fbc3abd76db8b471f856d78b74d
Pawel <pawel2antonik@gmail.com>
2018-09-02 15:47:16 +0200

    improvements in instrumental test

--- app/src/androidTest/java/com/hexfan/kotlinmvvm/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/hexfan/kotlinmvvm/ui/main/MainActivityTest.kt
@@ -3,17 +3,23 @@ package com.hexfan.kotlinmvvm.ui.main
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.content.Intent
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.hexfan.kotlinmvvm.R
 import com.hexfan.kotlinmvvm.TestApplication
 import com.hexfan.kotlinmvvm.common.domain.forecast.GetTodayForecastUseCase
 import com.hexfan.kotlinmvvm.common.domain.model.Forecast
 import com.hexfan.kotlinmvvm.common.domain.model.Weather
 import com.hexfan.kotlinmvvm.common.rx.IOTransformer
 import com.hexfan.kotlinmvvm.common.rx.RxTransformer
+import com.hexfan.kotlinmvvm.utils.ReactiveLocationProvider
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
-import io.reactivex.Flowable
 import io.reactivex.Single
 import org.junit.Before
 import org.junit.Rule
@@ -38,6 +44,7 @@ class MainActivityTest {
 
     val testCity = "TestCity"
     val forecast = Forecast(0, Weather(0, 0, 0, 0.0, "null", ""), testCity)
+
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
@@ -45,16 +52,17 @@ class MainActivityTest {
         viewModel = ForecastViewModelMock(data, interactor, IOTransformer())
         TestApplication.setInjector {
             (it as? MainActivity)?.factory = ForecastViewModelFactoryMock(viewModel)
+            (it as? MainActivity)?.reactiveLocationProvider = ReactiveLocationProvider(it)
         }
 
-        whenever(interactor.execute()).thenReturn(Single.just(forecast))
+        whenever(interactor.execute(any(), any())).thenReturn(Single.just(forecast))
         activityRule.launchActivity(Intent())
     }
 
     @Test
     fun showText() {
-//        data.postValue(forecast)
-//        onView(withId(R.id.cityTextView)).check(matches(withText(testCity)))
+        data.postValue(forecast)
+        onView(withId(R.id.textViewCity)).check(matches(withText(testCity)))
     }
 
     private class ForecastViewModelFactoryMock(val viewModel: MainViewModel) : MainViewModel.Factory(mock(), IOTransformer()) {
